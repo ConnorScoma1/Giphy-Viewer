4cefb69c771ed6e4477bb456b1e59539d6b40bce
connor scoma <connorscoma@connors-MacBook-Pro.local>
2019-05-12 22:42:07 -0500

    adding API img Div tag

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,35 @@
 
 
 const apikey = 'q7gYwhTD6jf37ZgdhpW9pAtkNEcsVS9Y';
-var gif = 'bucks'
 
 function buttonClicked() {
     var userInput = $('#userInput').val();
+
 }
 
 $(document).ready(function(){
 
+    $("#btn-submit").on("click", function(event) {
+        console.log("Submit Clicked")
+        event.preventDefault();
+        var gif = $('#userInput').val().trim();
+
+        console.log(userInput)
+
     $.ajax({
-        url: 'http://api.giphy.com/v1/gifs/search?q=' + gif + '&api_key=' + apikey,
+        url: 'https://api.giphy.com/v1/gifs/search?q=' + gif + '&api_key=' + apikey,
         type: 'GET',
     }).then(function(response){
         console.log(response)
 
+        for(var i = 0 ; i <= 25 ; i++) {
+            var urls = response.data[i].url // looping urls
+
+            $('.gif-container').prepend('<img class="gif-images" src="'+ urls + '">');
+            console.log(urls)
+        }
+
+    
+    })
     })
 })
